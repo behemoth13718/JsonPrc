0e8de1f91a78796f138b79fa6a57f172fb773471
Behemoth <behemoth13718@gmail.com>
2024-02-21 23:18:27 +0300

    изменил реквест

---
+++ SphaeraJsonRpc/Protocol/ModelMessage/RequestMessage/JsonRpcRequestServer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace SphaeraJsonRpc.Protocol.ModelMessage.RequestMessage
+{
+    public class JsonRpcRequestServer : JsonRpcRequest
+    {
+        [JsonProperty("params",  Order = 3)]
+        public object Params { get; set; }
+
+        [JsonIgnore]
+        public override EnumTypeMessage TypeMessage => EnumTypeMessage.Request;
+
+        //public override List<T> GetPayload<T>() => ((JArray)Params).ToObject<List<T>>();
+    }
+}
